85c53dd95e6b6af57bfd53589d5b9540626c3844
usaamatahir <usamatahir8330@gmail.com>
2022-01-09 04:55:11 +0500

    Added Preview Token

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENT_PREVIEW_TOKEN,
-        host: process.env.CONTENTFUL_HOST,
+        host: process.env.CONTENTFUL_HOST || "cdn.contentful.com",
       },
     },
     {
