6991e9cc3646c2dcb924402e078aee96caf0cdd7
usaamatahir <usamatahir8330@gmail.com>
2022-02-12 02:18:24 +0500

    Update code

--- src/Template/Policy.tsx
+++ src/Template/Policy.tsx
@@ -4,20 +4,16 @@ import { BlogProps } from "../utils/types";
 import { BLOCKS } from "@contentful/rich-text-types";
 import Layout from "../components/Common/Layout";
 import SEO from "../components/Common/SEO";
-import { graphql, Link } from "gatsby";
 import { url } from "../utils/utils";
 import { documentToPlainTextString } from "@contentful/rich-text-plain-text-renderer";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import getReadingTime from "../components/Common/readTime";
-import Form from "../components/Blog/Form";
 import { GatsbyImage } from "gatsby-plugin-image";
-
 import {
   Heading2,
   Heading3,
   Paragraph,
 } from "../components/Common/RichTextComponents";
-import SocialShare from "../components/Common/SocialShare";
 
 const Blog: FC<BlogProps> = ({ pageContext }) => {
   let count = 0;
@@ -56,7 +52,6 @@ const Blog: FC<BlogProps> = ({ pageContext }) => {
         path={url}
         createdAt={root.createdAt}
         updatedAt={root.updatedAt}
-        author={root.author.name}
         estTime={estTime}
         type={root.type}
       />
@@ -70,15 +65,6 @@ const Blog: FC<BlogProps> = ({ pageContext }) => {
                   JSON.parse(root.description.raw),
                   options
                 )}
-              <div className='flex flex-col md:flex-row md:items-center justify-between space-y-4 md:space-x-5 '>
-                <div className='font-poppins'>
-                  <span className='mr-2'>Author:</span>
-                  <Link to={`/author/${root.author.username}`}>
-                    <span className='text-primary'>{root.author.name}</span>
-                  </Link>
-                </div>
-                <SocialShare title={root.title} tags={root.tags} />
-              </div>
             </div>
           </div>
         </div>
